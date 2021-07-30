00eb23560a61d9705ef18232bdd0dc4ac9b00bcb
DELL <DELL@DESKTOP-SNS7GAF>
2021-07-30 08:56:22 +0700

    Commit ne

---
+++ src/demo.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class demo {
+	public static void main(String[] args) 
+	{
+        int num1,num2,num3;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap vao so thu nhat: ");
+        num1 = scanner.nextInt();
+        System.out.println("Nhap vao so thu 2: ");
+        num2 = scanner.nextInt();
+        System.out.println("Nhap vao so thu 3: ");
+        num3 = scanner.nextInt();
+        if( num1 >= num2 && num1 >= num3)
+            System.out.println(num1+" la so lon nhat!!");
+ 
+        else if (num2 >= num1 && num2 >= num3)
+            System.out.println(num2+" la so lon nhat!!");
+        else
+            System.out.println(num3+" la so lon nhat!!");
+    }
+}
